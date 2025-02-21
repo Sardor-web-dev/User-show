760bc436b831931c21e829ac65b44581b826365f
SarikViper <dzamolovsardor5@gmail.com>
2025-02-21 21:52:28 +0500

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-
-
-const users = [
-  { "id": 1, "name": "Alex Adams", "age": 23 },
-  { "id": 2, "name": "John Doe", "age": 25 },
-  { "id": 3, "name": "Emily Smith", "age": 22 },
-  { "id": 4, "name": "Michael Brown", "age": 30 },
-  { "id": 5, "name": "Sarah Johnson", "age": 27 },
-  { "id": 6, "name": "David Williams", "age": 24 },
-  { "id": 7, "name": "Laura Miller", "age": 26 },
-  { "id": 8, "name": "James Taylor", "age": 29 },
-  { "id": 9, "name": "Sophia Davis", "age": 21 },
-  { "id": 10, "name": "Daniel Moore", "age": 28 }
-]
+import {USERS} from './utils/users.js'
 
 
 function App() {
@@ -54,7 +41,7 @@ function App() {
       <div className='container'>
 
         {
-          users.map((user, idx) => (
+          USERS.map((user, idx) => (
             <div
               key={user.id}
               className="profile-container"
---
+++ src/utils/users.js
@@ -0,0 +1,12 @@
+export const USERS = [
+  { "id": 1, "name": "Alex Adams", "age": 23 },
+  { "id": 2, "name": "John Doe", "age": 25 },
+  { "id": 3, "name": "Emily Smith", "age": 22 },
+  { "id": 4, "name": "Michael Brown", "age": 30 },
+  { "id": 5, "name": "Sarah Johnson", "age": 27 },
+  { "id": 6, "name": "David Williams", "age": 24 },
+  { "id": 7, "name": "Laura Miller", "age": 26 },
+  { "id": 8, "name": "James Taylor", "age": 29 },
+  { "id": 9, "name": "Sophia Davis", "age": 21 },
+  { "id": 10, "name": "Daniel Moore", "age": 28 }
+]
