cd54c3eb2a953536c7590c92798d2ecd1bd193e8
SarikViper <dzamolovsardor5@gmail.com>
2025-02-24 16:40:52 +0500

    -

--- src/utils/users.js
+++ src/utils/users.js
@@ -1,22 +1,84 @@
 export const USERS = [
-  { "id": 1, "name": "Alex Adams", "age": 23 },
-  { "id": 2, "name": "John Doe", "age": 25 },
-  { "id": 3, "name": "Emily Smith", "age": 22 },
-  { "id": 4, "name": "Michael Brown", "age": 30 },
-  { "id": 5, "name": "Sarah Johnson", "age": 27 },
-  { "id": 6, "name": "David Williams", "age": 24 },
-  { "id": 7, "name": "Laura Miller", "age": 26 },
-  { "id": 8, "name": "James Taylor", "age": 29 },
-  { "id": 9, "name": "Sophia Davis", "age": 21 },
-  { "id": 10, "name": "Daniel Moore", "age": 28 },
-  { "id": 11, "name": "Olivia Harris", "age": 22 },
-  { "id": 12, "name": "Ethan Clark", "age": 31 },
-  { "id": 13, "name": "Ava Lewis", "age": 24 },
-  { "id": 14, "name": "Mason Walker", "age": 27 },
-  { "id": 15, "name": "Isabella Allen", "age": 23 },
-  { "id": 16, "name": "Lucas Young", "age": 26 },
-  { "id": 17, "name": "Mia Hall", "age": 25 },
-  { "id": 18, "name": "William Scott", "age": 28 },
-  { "id": 19, "name": "Charlotte King", "age": 29 },
-  { "id": 20, "name": "Henry Wright", "age": 30 }
+  
+  { "id": 1,
+     "name": "Alex Adams",
+     "age": 23 
+  },
+  { "id": 2,
+     "name": "John Doe",
+     "age": 25 
+  },
+  { "id": 3,
+     "name": "Emily Smith",
+     "age": 22 
+  },
+  { "id": 4,
+     "name": "Michael Brown",
+     "age": 30 
+  },
+  { "id": 5,
+     "name": "Sarah Johnson",
+     "age": 27 
+  },
+  { "id": 6,
+     "name": "David Williams",
+     "age": 24 
+  },
+  { "id": 7,
+     "name": "Laura Miller",
+     "age": 26 
+  },
+  { "id": 8,
+     "name": "James Taylor",
+     "age": 29 
+  },
+  { "id": 9,
+     "name": "Sophia Davis",
+     "age": 21 
+  },
+  { "id": 10,
+     "name": "Daniel Moore",
+     "age": 28 
+  },
+  { "id": 11,
+     "name": "Olivia Harris",
+     "age": 22 
+  },
+  { "id": 12,
+     "name": "Ethan Clark",
+     "age": 31 
+  },
+  { "id": 13,
+     "name": "Ava Lewis",
+     "age": 24 
+  },
+  { "id": 14,
+     "name": "Mason Walker",
+     "age": 27 
+  },
+  { "id": 15,
+     "name": "Isabella Allen",
+     "age": 23 
+  },
+  { "id": 16,
+     "name": "Lucas Young",
+     "age": 26 
+  },
+  { "id": 17,
+     "name": "Mia Hall",
+     "age": 25 
+  },
+  { "id": 18,
+     "name": "William Scott",
+     "age": 28 
+  },
+  { "id": 19,
+     "name": "Charlotte King",
+     "age": 29 
+  },
+  { "id": 20,
+     "name": "Henry Wright",
+     "age": 30 
+  }
+  
 ];
